78726c800b1883a35f8a453605d90d478e5c4a13
Joseph Thompson <jthompson182@bcst01.parkland.edu>
2022-04-28 14:55:35 -0500

    first commit of all files

---
+++ welcomescreen.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--  I honor Parkland's core values by affirming that I have
+followed all academic integrity guidelines for this work.
+Your name
+Your section -->
+<html>
+<head>
+
+<?php readfile("header.html"); ?>
+
+</head>
+<body>
+<h1> WELCOME </h1>
+</body>
+</html>
+
